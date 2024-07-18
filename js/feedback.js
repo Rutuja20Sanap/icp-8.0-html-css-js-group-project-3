9907e28ecce7fa11ba4f960df63d98f2213b9fdb
22piyush <piyushaglawe05@gmail.com>
2024-07-18 17:00:50 +0530

    done feedback page

---
+++ js/feedback.js
@@ -0,0 +1,23 @@
+
+function Light(active) {
+    const lightElement = document.getElementById(active);
+    if (lightElement.src.includes('star-color.png')) {
+        lightElement.src = '../images/feedback-signin/star.png';
+    } else {
+        lightElement.src = '../images/feedback-signin/star-color.png';
+    }
+}
+
+function Submit() {
+
+    const element = document.getElementById('text-area');
+     const star = document.getElementById("img1")
+    if(element.value == 0)
+    {
+        alert("Filled text area");
+    }
+    else{
+        alert("Thank you");
+    }
+    
+}
