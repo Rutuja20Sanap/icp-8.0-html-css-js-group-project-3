0537193ee40d0bc1997357359348487bd32d1bb1
karishmawadaskar <karishmawadaskar@gmail.com>
2024-07-19 00:48:37 -0800

    added correct answer list of all que. in js file

---
+++ js/fun-quizz.js
@@ -0,0 +1,23 @@
+//show the number of que, active question and intial score
+const totalQuestions = 15;
+let activeQuestionIndex = 0;
+let score = 0;
+
+//All correct answer 
+const correctAnswers = {
+    question1: "Happiness",
+    question2: "Sad",
+    question3: "Angry",
+    question4: "Confused",
+    question5: "Relaxed",
+    question6: "Embarrassment",
+    question7: "Scared",
+    question8: "Grumpy",
+    question9: "Naughty",
+    question10: "Sad,but also you are piece of poop",
+    question11: "Starbucks",
+    question12: "Toad in the hole",
+    question13: "Pear",
+    question14: "Pineapple",
+    question15: "Shepards pie"
+}
