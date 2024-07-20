aa4ea96bb6badb4e3446d16b748a76e3d052f8bc
Rutuja20Sanap <dnyaneshwarsanap293@gmail.com>
2024-07-20 05:24:53 -0930

    add js for open and close menu bar

--- js/header.js
+++ js/header.js
@@ -7,4 +7,10 @@ function toggleTheme() {
     }
 }
 
-   
+    document.getElementById('menu-open').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.add('open');
+    });
+
+    document.getElementById('close-btn').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.remove('open');
+    });
