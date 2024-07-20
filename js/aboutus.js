85193690af3303991724aca5322b3e008bd661b0
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-07-20 19:43:45 +0530

    dark mode light mode added

---
+++ js/aboutus.js
@@ -0,0 +1,12 @@
+function enabledark(){
+    const mainbody = document.getElementById("main-body")
+    mainbody.classList.remove("bg-light")
+    mainbody.classList.add("bg-dark")
+
+  }
+  function enablelight(){
+    const mainbody = document.getElementById("main-body")
+    mainbody.classList.remove("bg-dark")
+    mainbody.classList.add("bg-light")
+
+  }
