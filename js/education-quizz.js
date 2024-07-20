631228caa8b2feec8b126258f62b3a48787e3a65
SuhasDhawale <suhasdhawale57@gmail.com>
2024-07-20 16:50:39 +0530

    created variables for elements

--- js/education-quizz.js
+++ js/education-quizz.js
@@ -5,3 +5,12 @@ const questions = [
         correctAnswer: "H2O"
     }];
 
+let currentQuestionIndex = 0;
+let score = 0;
+const questionEl = document.getElementById('question');
+const choicesEl = document.getElementById('choices');
+const nextButton = document.getElementById('next-button');
+const prevButton = document.getElementById('prev-button');
+const resultContainer = document.getElementById('result-container');
+const quizContainer = document.getElementById('quiz-container');
+const scoreEl = document.getElementById('score');
