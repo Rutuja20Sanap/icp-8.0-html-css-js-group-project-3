39ac098ab84e65c49f74d5e3d9d86ae4fd2e0d74
SuhasDhawale <suhasdhawale57@gmail.com>
2024-07-21 01:48:37 +0530

    added toggle function

--- js/education-quizz.js
+++ js/education-quizz.js
@@ -152,3 +152,20 @@ function restartQuiz() {
 }
 
 loadQuestion();
+
+function toggleTheme() {
+    const body = document.getElementById('body');
+    if (body.classList.contains('dark-theme')) {
+        body.classList.replace('dark-theme', 'light-theme');
+    } else {
+        body.classList.replace('light-theme', 'dark-theme');
+    }
+}
+
+    document.getElementById('menu-open').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.add('open');
+    });
+
+    document.getElementById('close-btn').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.remove('open');
+    });
