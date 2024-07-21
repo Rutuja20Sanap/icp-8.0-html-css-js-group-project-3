44184536c61ddff03589dcaf0ae70c6162d0ee69
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-07-21 23:08:22 +0530

    java script file is added

--- js/game.js
+++ js/game.js
@@ -1,40 +1,45 @@
-const emojis = [`🐶`,`🐭`,`🦊`,`🐻`,`🦁`,`🐸`,`🐷`,`🐥`,`🐶`,`🐭`,`🦊`,`🐻`,`🦁`,`🐸`,`🐷`,`🐥`];
+const emojis = ['😍', '😎', '😎', '😇', '😍', '😇', '😈', '😈','😍', '😎', '😎', '😇', '😍', '😇', '😈', '😈'];
 
-let change_emojis = emojis.sort(ChangeEmojies);
-function ChangeEmojies(){
-    if(Math.random() > 0.5){
-        return  2 ;
-    }else{
-        return -1 ;
-    }  
+let shuf_emojis = emojis.sort(shuffleEmojis)
+
+function shuffleEmojis(){
+if(Math.random() > 0.5){
+ return  2 ;
+}else{
+    return -1 ;
 }
-for (let i = 0; i < emojis.length; i++){
-    let box = document.createElement('div');
-    box.className = 'item';
-    box.innerHTML = change_emojis[i];
+}
+
+for (let i = 0; i < emojis.length; i++) {
+     let box = document.createElement('div');
+     box.className = 'item';
+    box.innerHTML = shuf_emojis[i];
 
     box.onclick = function() {
         this.classList.add('boxOpen');
-
+        
         setTimeout(function() {
 
             if (document.querySelectorAll('.boxOpen').length > 1) {
-                if (document.querySelector('.boxOpen')[0].innerHTML == document.querySelector('.boxOpen')[1].innerHTML) {
+                if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML) {
 
-                    document.querySelector('.boxOpen')[0].classList.add('boxMatch');
-                    document.querySelector('.boxOpen')[1].classList.add('boxMatch');
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch');
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch');
 
-                    document.querySelector('.boxOpen')[1].classList.remove('boxOpen');
-                    document.querySelector('.boxOpen')[0].classList.remove('boxOpen');
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
 
-                    if (document.querySelector('.boxMatch').length == emojis.length) {
+                    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
                         alert('win');
                     }
-                }else {
-                        document.querySelector('.boxOpen')[1].classList.remove('boxOpen');
-                        document.querySelector('.boxOpen')[0].classList.remove('boxOpen');
-                    }
+
+                } else {
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
                 }
-                }, 500);
+            }
+
+        }, 500);
     };
-} 
+    document.querySelector('.game').appendChild(box);
+}
