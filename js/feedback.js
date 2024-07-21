d10aa7c1e728beea28d4cc10ad228b541d9c3742
22piyush <piyushaglawe05@gmail.com>
2024-07-21 11:44:43 +0530

    done

--- js/feedback.js
+++ js/feedback.js
@@ -14,17 +14,33 @@ function Submit() {
     const textShow = document.getElementById('review-text');
     const messageShow = document.getElementById('msg-show');
     const star = document.getElementById("img1")
-    if(element.value == 0)
+    const rev = document.getElementById("review-1");
+
+    const inputBoxx = document.getElementById("input-boxx")
+    const nameReview = document.getElementById("name-review");
+    
+
+    if(element.value == 0 || inputBoxx.value==0)
     {
         messageShow.innerText = 'Fillled form';
+        messageShow.style.color = 'red';
+        rev.style.display = 'none';
     }
     else{
         messageShow.innerText = 'Thank You🙂';
+        messageShow.style.color = 'green';
+        rev.style.display = 'block';
     }
      
     textShow.innerText = element.value;
     element.value = " ";
 
+    nameReview.innerText = inputBoxx.value;
+    inputBoxx.value = " ";
+
+    
+    
+   
      
 }
 
