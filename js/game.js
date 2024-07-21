a0160815c11e3a0f55aa6b803f40a325df9918c9
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-07-21 22:55:33 +0530

    box are open but not a same two box then remove a open box class

--- js/game.js
+++ js/game.js
@@ -27,9 +27,14 @@ for (let i = 0; i < emojis.length; i++){
                     document.querySelector('.boxOpen')[1].classList.remove('boxOpen');
                     document.querySelector('.boxOpen')[0].classList.remove('boxOpen');
 
-                    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
+                    if (document.querySelector('.boxMatch').length == emojis.length) {
                         alert('win');
                     }
-                }}
+                    else {
+                        document.querySelector('.boxOpen')[1].classList.remove('boxOpen');
+                        document.querySelector('.boxOpen')[0].classList.remove('boxOpen');
+                    }
+                }
+                }
     })
 } }
