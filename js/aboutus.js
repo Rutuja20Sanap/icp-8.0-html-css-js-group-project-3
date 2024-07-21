279b0606b245e2cb6a9f1c68c8b687f48358363e
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-07-21 16:06:48 +0530

    one game created

--- js/aboutus.js
+++ js/aboutus.js
@@ -21,3 +21,20 @@ function enabledark(){
         alert('You are not eligible to play the games.');
     }
 }
+function countAlphabets() {
+  const text = document.getElementById('textInput').value;
+  const alphabetsCount = text.replace(/[^a-zA-Z]/g, '').length;
+  const userAnswer = parseInt(document.getElementById('alphabetCountInput').value, 10);
+
+  if (userAnswer === alphabetsCount) {
+      document.getElementById('result1').innerText = 'Correct!';
+  } else {
+      document.getElementById('result1').innerText = `Wrong! There are ${alphabetsCount} alphabets.`;
+  }
+}
+
+function resetAlphabetsGame() {
+  document.getElementById('textInput').value = '';
+  document.getElementById('alphabetCountInput').value = '';
+  document.getElementById('result1').innerText = '';
+}
