173e40790fcd95014a32be46683d7081ca714d21
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-07-25 11:51:02 +0530

    checkwinner function is added

--- js/game2.js
+++ js/game2.js
@@ -25,4 +25,19 @@ function handleClick(box) {
     [3,4,5],
     [6,7,8]
   ];
+
+  function checkWinner() {
+    for (let pattern of winPattern) {
+      let pos1Val = boxes[pattern[0]].innerText;
+      let pos2Val = boxes[pattern[1]].innerText;
+      let pos3Val = boxes[pattern[2]].innerText;
   
+      if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if (pos1Val === pos2Val && pos2Val === pos3Val) {
+          console.log("winner", pos1Val);
+          showWinner(pos1Val);
+          return true;
+        }
+      }
+    }
+  }
