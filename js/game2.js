ad3fed7cc8ecb68a9bbe70e839872a72085d43a7
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-07-25 11:48:46 +0530

    win pattern

--- js/game2.js
+++ js/game2.js
@@ -12,5 +12,17 @@ function handleClick(box) {
         turno = true;
       }
     box.disabled = true;
-    
+
   }
+
+  let winPattern= [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+  ];
+  
